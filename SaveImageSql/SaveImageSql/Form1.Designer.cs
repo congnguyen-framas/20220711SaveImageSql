b5bf4d2c6e79c7b28be9178157580d79a2d52a96
congnguyen-framas <68525123+congnguyen-framas@users.noreply.github.com>
2022-07-11 16:12:34 +0700

    ok

--- SaveImageSql/SaveImageSql/Form1.Designer.cs
+++ SaveImageSql/SaveImageSql/Form1.Designer.cs
@@ -29,13 +29,84 @@ namespace SaveImageSql
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.gridData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 55);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Upload";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(394, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1498, 1017);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File name";
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(12, 29);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(221, 20);
+            this.txtFilename.TabIndex = 3;
+            // 
+            // gridData
+            // 
+            this.gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridData.Location = new System.Drawing.Point(12, 112);
+            this.gridData.Name = "gridData";
+            this.gridData.Size = new System.Drawing.Size(376, 247);
+            this.gridData.TabIndex = 4;
+            this.gridData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridData_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.gridData);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.DataGridView gridData;
     }
 }
 
--- SaveImageSql/SaveImageSql/Form1.cs
+++ SaveImageSql/SaveImageSql/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +17,90 @@ namespace SaveImageSql
     {
         public Form1()
         {
+            Load += Form1_Load;
+
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void InsertImage(string fileName, byte[] image)
+        {
+            using (var connection = new SqlConnection("Data Source=192.168.180.210;Initial Catalog=RecycledAppTEST;User ID=readmin;Password=Recycle123$;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                var param = new DynamicParameters();
+                param.Add("@fileName", fileName);
+                param.Add("@image", image);
+                var result = connection.Execute("Insert into tblImages (FileName, Image) values (@fileName,@image)", param);
+            }
+        }
+
+        private void LoadData()
+        {
+            List<ImageModel> result;
+            using (var connection = new SqlConnection("Data Source=192.168.180.210;Initial Catalog=RecycledAppTEST;User ID=readmin;Password=Recycle123$;Connect Timeout=30"))
+            {
+                result = connection.Query<ImageModel>("select * from tblImages").ToList();
+                gridData.DataSource = result;
+            }
+        }
+
+        private byte[] convertImageToByte(Image img)
+        {
+            using (var ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        private Image ConvertByteArrayToimage(byte[] data)
+        {
+            using (var ms = new MemoryStream(data))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //"Text files (*.txt)|*.txt|All files (*.*)|*.*"'
+            using (var ofd = new OpenFileDialog() { Filter = "*Image file (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*", Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //display image to picture box
+                    pictureBox1.Image = Image.FromFile(ofd.FileName);
+                    txtFilename.Text = ofd.FileName;
+                    InsertImage(ofd.FileName, convertImageToByte(pictureBox1.Image));
+
+                    LoadData();
+                }
+            }
+        }
+
+        private void gridData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataTable dt = gridData.DataSource as DataTable;
+
+            List< ImageModel> res = (List< ImageModel>)gridData.DataSource;
+
+            if (res != null)
+            {
+                
+                pictureBox1.Image = ConvertByteArrayToimage((byte[])res[e.RowIndex].Image);
+            }
+        }
+    }
+
+    public class ImageModel
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public byte[] Image { get; set; }
+
     }
 }
